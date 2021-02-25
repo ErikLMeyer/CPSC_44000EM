9fbbb390898b67fd00f740472951914b7b3947d3
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-24 20:22:52 -0600

    Initial commit

---
+++ portfolio.js
@@ -0,0 +1,16 @@
+function myGit(repo){
+    var myHub = "https://github.com/ErikLMeyer/xxx";
+    var myRepo = myHub.replace("xxx", repo);
+    return myRepo;
+}
+
+function sprint3(){
+    var chapter = '';
+    var seSprint3 = '';
+    for (i = 2; i <= 4; i++){
+        chapter = "exercise" + i;
+        sePrint3 = "SE-Sprint3-Exercises-Chapter-" + i;
+        document.getElementById(chapter).href = myGit(seSprint3);
+        document.getElementById(chapter).title = chapter;
+    }
+}
