55765c651d5a768b5dcc364fc82835a6d152d603
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-24 20:36:27 -0600

    Again

--- portfolio.js
+++ portfolio.js
@@ -4,15 +4,7 @@ function myGit(repo){
     return myRepo;
 }
 
-/*
+
 function sprint3(){
-    var chapter = '';
-    var seSprint3 = '';
-    for (i = 2; i <= 4; i++){
-        chapter = "exercise" + i;
-        sePrint3 = "SE-Sprint3-Exercises-Chapter-" + i;
-        document.getElementById(chapter).innerHTML = myGit(seSprint3);
-        document.getElementById(chapter).title = chapter;
-    }
+    document.getElementById("exercise4").href = myGit("SE-Sprint3-Exercises-Chapter-4");
 }
-*/
