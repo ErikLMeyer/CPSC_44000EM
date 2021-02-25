95fe84a87374f6a5b3437a3b4bc55cae0820a124
ErikLMeyer <erikmeyer@lewisu.edu>
2021-02-25 14:38:09 -0600

    New method added

--- portfolio.js
+++ portfolio.js
@@ -1,15 +1,23 @@
+// for when I want to link a git repository.
 function myGit(repo){
     var myHub = "https://github.com/ErikLMeyer/xxx";
     var myRepo = myHub.replace("xxx", repo);
     return myRepo;
 }
 
+// for when I want to link to a deployed project.
+function azure(siteName){
+    var azure = "https://xxx.azurewebsites.net";
+    var correctAzure = azure.replace("xxx", siteName);
+    return correctAzure;
+}
+
 
 function sprint3(){
-    document.getElementById("exercise2").href = myGit("SE-Sprint3-Exercises-Chapter-2");
+    document.getElementById("exercise2").href = azure("sprint3-chapter2");
     document.getElementById("exercise2").title = "Chapter 2";
-    document.getElementById("exercise3").href = myGit("SE-Sprint3-Exercises-Chapter-3");
+    document.getElementById("exercise3").href = azure("sprint3-chapter3");
     document.getElementById("exercise2").title = "Chapter 3"
-    document.getElementById("exercise4").href = myGit("SE-Sprint3-Exercises-Chapter-4");
+    document.getElementById("exercise4").href = azure("sprint3-chapter4");
     document.getElementById("exercise2").title = "Chapter 4";
 }
